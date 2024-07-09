5b2d8dbbd192fb4540b2e51dba403e3caab4c4a9
farz1212 <farzaad1212@gmail.com>
2024-07-08 21:54:10 -0400

    Initial Commit

---
+++ TikTacToe/TikTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTacToeApp.swift
+//  TikTacToe
+//
+//  Created by Farzaad Goiporia on 2024-07-08.
+//
+
+import SwiftUI
+
+@main
+struct TikTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
